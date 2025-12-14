e5e4688c46f2bedf9eb9a9f2f6f96e4dec1a4eda
fowl_1 <132679896+fowl-dev@users.noreply.github.com>
2025-12-14 21:28:53 +0530

    Inital commit

---
+++ binge.py
@@ -0,0 +1,21 @@
+def binge(order):
+    k=sorted(order)
+    print(k)
+    max_con=[]
+    temp=[k[0]]
+    for i in range(len(k)-1):
+        if k[i]+1==k[i+1]: 
+            temp.append(k[i+1])
+        else:                     
+            if temp:             
+                max_con.append(temp)
+            temp=[k[i+1]]    
+    if temp:                     
+        max_con.append(temp)
+    res=sorted(max_con,key=lambda x:len(x),reverse=True)
+    if len(res[0])==1:
+        return "No consecutive watch order."
+    else:
+        return res[0]
+List=eval(input("Watched episodes: "))
+print(binge(List))
